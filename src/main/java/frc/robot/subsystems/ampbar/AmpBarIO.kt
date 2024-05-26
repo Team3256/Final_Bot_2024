5c6d6a2df33ce036c1583e8e3e872edce125de88
ThatXliner <bryan.hu.2020@gmail.com>
2024-05-26 16:25:29 -0700

    Kotlinify AmpBar

---
+++ src/main/java/frc/robot/subsystems/ampbar/AmpBarIO.kt
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+package frc.robot.subsystems.ampbar
+
+import org.littletonrobotics.junction.AutoLog
+
+interface AmpBarIO {
+    @AutoLog
+    class AmpBarIOInputs {
+        var ampBarMotorVoltage: Double = 0.0
+        var ampBarMotorPosition: Double = 0.0
+        var ampBarMotorVelocity: Double = 0.0
+        var ampBarMotorStatorCurrent: Double = 0.0
+        var ampBarMotorSupplyCurrent: Double = 0.0
+        var ampBarMotorTemperature: Double = 0.0
+    }
+
+    fun updateInputs(inputs: AmpBarIOInputs?) {}
+
+    fun setVoltage(voltage: Double) {}
+
+    val isCurrentSpiking: Boolean
+        get() = false
+
+    fun off() {}
+}
